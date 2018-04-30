46e19a6941487d317b70dd8a2cfebd220b9e00dd
jesus <jesus.a.d.a@hotmail.com>
2018-04-30 16:13:24 +0200

    api and handle bars

--- src/server/index.js
+++ src/server/index.js
@@ -5,15 +5,35 @@ import path from 'path';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import open from 'open';
+import exphbs from 'express-handlebars';
 
 // Webpack Configuration
 import webpackConfig from '../../webpack.config.dev';
+
+// API
+import blogApi from './api/blog';
+
+// Helpers
+import * as hbsHelper from '../lib/handlebars';
+
+// Utils
+import { isMobile } from '../lib/utils/device';
 
 //Server port
 const port = 3000;
 
 // Express app
 const app = express();
+
+// Handlebars setup
+app.engine('.hbs', exphbs({
+    extname: '.hbs',
+    helpers: hbsHelper
+}));
+
+// View Engine Setup
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', '.hbs');
 
 // Webpack Compiler
 const webpackCompiler = webpack(webpackConfig);
@@ -22,10 +42,23 @@ const webpackCompiler = webpack(webpackConfig);
 app.use(webpackDevMiddleware(webpackCompiler));
 app.use(webpackHotMiddleware(webpackCompiler));
 
+// Device detector
+app.use((req, res, next) => {
+    res.locals.isMobile = isMobile(req.headers['user-agent']);
+
+    return next();
+});
+
+// API dispatch
+app.use('/api/blog', blogApi);
+
+// Sending all the traffic to React
 // Sending all the traffic to React
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+    res.render('frontend/index', {
+      layout: false
+    });
+  });
 
 // Listen port 3000
 app.listen(port, err => {
